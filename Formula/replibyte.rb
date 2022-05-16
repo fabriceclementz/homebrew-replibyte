03bd8d00f8f13a7c97680daf6af15e9d7ec8bd58
Fabrice Clementz <fabriceclementz@users.noreply.github.com>
2022-05-16 22:51:36 +0200

    Update replibyte.rb

--- Formula/replibyte.rb
+++ Formula/replibyte.rb
@@ -1,9 +1,9 @@
 class Replibyte < Formula
-  version "0.0.13"
-  desc "Synchronize your cloud databases and hide sensitive data 🕵️‍♀️"
+  version "0.6.3"
+  desc "Seed Your Development Database With Real Data ⚡️"
   homepage "https://github.com/Qovery/replibyte"
   url "https://github.com/Qovery/replibyte/releases/download/v#{version}/replibyte_v#{version}_x86_64-apple-darwin.zip"
-  sha256 "e6723fa2878f15c53877e7fd0bdba98d785ccf171fcf1fdbafee0cdafd00a952"
+  sha256 "db1bed1a7a94ed2d594f4ba9f1613a04bed1e2f606c2af77a2f8fa709c8bbc9d"
   license "MIT"
 
   def install
